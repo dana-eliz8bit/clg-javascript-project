bd2b96869409d3079a3040a30e6e3595fcea448c
dana.eliz8bit <94809868+dana-eliz8bit@users.noreply.github.com>
2022-11-10 11:18:09 +1000

    Add book list project.

--- js/main.js
+++ js/main.js
@@ -67,10 +67,7 @@ guessBtn.addEventListener("click", function () {
     guessesLeft -= 1;
     if (guessesLeft === 0) {
       // Game Over - Loss
-      gameOver(
-        false,
-        `GAME OVER! It was ${winningNum}!`
-      );
+      gameOver(false, `GAME OVER! It was ${winningNum}!`);
     } else {
       // Game Continues - Wrong Number
       // Change Border Color
@@ -78,10 +75,7 @@ guessBtn.addEventListener("click", function () {
       // Clear Input
       guessInput.value = "";
       // Message
-      setMessage(
-        `OOPS! ${guessesLeft} of 3 guesses left!`,
-        "#f1592c"
-      );
+      setMessage(`OOPS! ${guessesLeft} of 3 guesses left!`, "#f1592c");
     }
   }
 });
@@ -168,3 +162,116 @@ const displayData = (data) => {
 };
 
 fetchNASAData();
+
+// BOOK LIST
+
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  addBookToList(book) {
+    const list = document.getElementById("book-list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td class="hidden">${book.isbn}</td>
+      <td><a href="#" class="delete">X<a></td>
+    `;
+    list.appendChild(row);
+  }
+  showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container-book-list");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+    setTimeout(function () {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
+  deleteBook(target) {
+    if (target.className === "delete") {
+      target.parentElement.parentElement.remove();
+    }
+  }
+  clearFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+  }
+}
+
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function (book) {
+      const ui = new UI();
+      ui.addBookToList(book);
+    });
+  }
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
+// DOM Load Event
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
+
+// Event Listener - Add Book
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  const book = new Book(title, author, isbn);
+
+  const ui = new UI();
+  console.log(ui);
+
+  if (title === "" || author === "" || isbn === "") {
+    ui.showAlert("All fields required!", "error");
+  } else {
+    ui.addBookToList(book);
+    Store.addBook(book);
+    ui.showAlert("Book Added!", "success");
+    ui.clearFields();
+  }
+  e.preventDefault();
+});
+
+// Event Listener for Delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  const ui = new UI();
+  ui.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  ui.showAlert("Book Removed!", "success");
+  e.preventDefault();
+});
